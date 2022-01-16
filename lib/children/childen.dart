80f7fdb1201635aa3068040bdd0b4ebf98d101bf
nik <kolxz@mail.ru>
2022-01-16 18:02:17 +0300

    Initial commit

---
+++ lib/children/childen.dart
@@ -0,0 +1,62 @@
+class Stud{
+  static const int SIZE = 10;
+  final List<int> _grades = [SIZE];
+  late String _name;
+  int _gradesCount = 0;
+
+  Stud(String name){
+    if (name.isNotEmpty) {
+      _name = name;
+    } else {
+      return;
+    }
+  }
+
+  void setGrades(List<int> grade){
+    if (SIZE - _gradesCount >= grade.length){
+      for(int i in grade) {
+        if (i > 1 && i < 6) {
+          _grades.add(i);
+          _gradesCount++;
+        } else {
+          return;
+        }
+      }
+    } else {
+      return;
+    }
+  }
+
+  bool setGradeByIndex(int grade, int index){
+    if (index > -1 && index <= _gradesCount) {
+      if (grade > 1 && grade < 6) {
+        _grades[index] = grade;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  int getGrade(int index){
+    if (index > -1 && index <= _gradesCount) {
+      return _grades[index];
+    } else {
+      return 0;
+    }
+  }
+
+  double getAverage (){
+    if (_gradesCount != 0) {
+      double sum = 0;
+      for(int i in _grades){
+        sum += i;
+      }
+      return sum / _gradesCount;
+    }
+    return 0.0;
+  }
+
+  String getName() => _name;
+
+  List<int> getGrades() => _grades;
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,148 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:to_do_list/children/childen.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  // Фреймворк вызывает createState()когда он хочет построить виджет
+  State<Home> createState() => _HomeState();
+}
+// StatefulWidgets для динамических значений
+// класс хранит изменяемые данные которые могут
+// меняться в течение жизни виджета.
+class _HomeState extends State<Home> {
+
+  late String _userTodo;
+  List<Stud> todoList = [Stud("name")];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[900], // насыщенность цвета [900]
+      appBar: AppBar(
+        title: Text("Quantity of students: " + todoList.length.toString()),
+        centerTitle: true,
+      ),
+      // отображение элементов в виде списка
+      body: ListView.builder(
+          // сколько элементов в списке
+          itemCount: todoList.length,
+          // как будет построен список. Перебирает всесь список и позволяет
+          // раотать с каждым элементом в отдельности. Задаётся функцией
+          itemBuilder: (BuildContext context, int index){
+            // возвращает класс позволяющий смахивать в право или в лево удаляя
+            // элемент в key принимает элемент с которым работаем в данный
+            // момент, в child прописываем дизайн в котором мы прописываем
+            // представление элементов
+            return Dismissible(key: Key(todoList[index].getName()),
+                // класс представления ввиде карточки
+                child: Card(
+                  // в карточку выводим только заголовок
+                  child: ListTile(
+                      leading: Text(
+                        todoList[index].getName(),
+                        style: const TextStyle(
+                          fontSize: 20
+                        ),
+                      ),
+                      title: Stack(
+                        children: <Widget>[
+                          Align(
+                            alignment:  Alignment(0.8, 0.5),
+                            child:
+                              IconButton(
+                              icon: const Icon(
+                                Icons.info,
+                                color: Colors.pink,
+                              ),
+                              onPressed: (){
+                                setState(() {
+                                todoList.removeAt(index);
+                                });
+                              }
+                              ),
+                          ),
+                          Align(
+                            alignment:  Alignment(0.8, 0.5),
+                            child:
+                            IconButton(
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.red,
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    todoList.removeAt(index);
+                                  });
+                                }
+                            ),
+                          ),
+                          Align(
+                            alignment:  Alignment(1, 0.5),
+                            child:
+                            IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.deepPurpleAccent,
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    todoList.removeAt(index);
+                                  });
+                                }
+                            ),
+                          ),
+                        ]
+                      )
+                  ),
+                ),
+                onDismissed: (direction){
+              // проверка что свайп был с права на лево
+              // if (direction == DismissDirection.endToStart)
+                  setState(() {
+                    todoList.removeAt(index);
+                  });
+                },
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        onPressed: (){
+          // показываеем диалоговое окно. Принимает 2 параметра.
+          // 1) Контекст. Поверх чего будет отобразаться окно.
+          // Context берём из входного параметра
+          // Widget build(BuildContext context)
+          // 2)
+          showDialog(context: context, builder: (BuildContext context){
+            return AlertDialog(
+              title: const Text(' Add name'),
+              content: TextField(
+                onChanged: (String value){
+                  _userTodo = value;
+                },
+              ),
+              actions: [
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    todoList.add(Stud(_userTodo));
+                  });
+                  // после нажатия скрывает диалоговЫЕ окнА
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Add"))
+              ],
+            );
+          });
+        },
+        child: const Icon(
+          Icons.add_box,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
